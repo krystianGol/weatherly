772b211610b2f6d6a093c9c3e21e2e9263af8916
Krystian <krystianrafalgolda@gmail.com>
2024-03-10 15:55:00 +0100

    modified index.js index.ejs

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import express from 'express'
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_KEY = "0227f9f67a8f81150432bd9213ffc06d";
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+        res.render("index.ejs", { data: JSON.stringify(response.data) });
+    } catch (error) {
+        res.render("index.ejs", { data: JSON.stringify(error.response.data) });
+    }
 })
 
 app.listen(port, () => {
