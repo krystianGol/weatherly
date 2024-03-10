30e173734be6a4d1c3a64fcb1a6ba96cce3cc272
Krystian <krystianrafalgolda@gmail.com>
2024-03-10 18:16:03 +0100

    modified index.js index.ejs add folder style  modified style.css

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const port = 3000;
 const API_KEY = "0227f9f67a8f81150432bd9213ffc06d";
 const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
+app.get("/", (req, res) => {
+    res.render('index.ejs');
+})
+
+app.post('/weather', async (req, res) => {
     try {
         const response = await axios.get(API_URL);
-        res.render("index.ejs", { data: JSON.stringify(response.data) });
+        //response.data.list[0].weather[0].description
+        console.log(response.data.list[0].dt_txt);
+        res.render("index.ejs", { data: req.body.city });
     } catch (error) {
-        res.render("index.ejs", { data: JSON.stringify(error.response.data) });
+        res.render("index.ejs", { data: "fail" });
     }
 })
 
