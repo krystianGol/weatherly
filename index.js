95cb52b0593419ea3f9193cfe96c67c4dfc40a0c
Krystian <krystianrafalgolda@gmail.com>
2024-03-11 15:18:17 +0100

    finished main functionality

--- index.js
+++ index.js
@@ -5,21 +5,60 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const API_KEY = "0227f9f67a8f81150432bd9213ffc06d";
-const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
+const currentDate = new Date();
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDayIndex = currentDate.getDay();
+let nextDaysOfWeek = [];
+nextDaysOfWeek.push(daysOfWeek[currentDayIndex]);
+
+let data;
+let city;
+let currentWeathericonURL;
+let forecastIconUrls = [];
+let fiveDaysForecastIconsUrl = [];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render('index.ejs');
+    for (let i = 1; i < 6; i++) {
+        const nextDayIndex = (currentDayIndex + i) % 7;
+        nextDaysOfWeek.push(daysOfWeek[nextDayIndex]);
+    }
+
+    res.render('index.ejs', {
+        data: data,
+        city: city,
+        daysOfTheWeek: nextDaysOfWeek,
+        currentWeathericonURL: currentWeathericonURL,
+        forecastIconUrls: forecastIconUrls,
+        fiveDaysForecastIconsUrl: fiveDaysForecastIconsUrl,
+    });
 })
 
 app.post('/weather', async (req, res) => {
+    forecastIconUrls = [];
+    fiveDaysForecastIconsUrl = [];
+
+    city = req.body.city;
+    const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
     try {
         const response = await axios.get(API_URL);
         //response.data.list[0].weather[0].description
-        console.log(response.data.list[0].dt_txt);
-        res.render("index.ejs", { data: req.body.city });
+        data = response.data;
+
+        currentWeathericonURL = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`;
+
+        for (let i = 0; i < 8; i++) {
+            forecastIconUrls.push(`https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`);
+        }
+
+        for (let i = 0; i < 5; i++) {
+            fiveDaysForecastIconsUrl.push(`https://openweathermap.org/img/wn/${data.list[i * 8].weather[0].icon}@2x.png`)
+        }
+
+
+        res.redirect("/");
     } catch (error) {
         res.render("index.ejs", { data: "fail" });
     }
